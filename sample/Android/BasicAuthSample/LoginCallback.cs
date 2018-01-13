ec915b16d04060b68963f6313b121ba00319beea
Andy <witan03@ca.com>
2018-01-12 16:14:59 -0800

    Add Android Sample

---
+++ sample/Android/BasicAuthSample/LoginCallback.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Android.App;
+using Com.CA.Mas.Foundation;
+using Java.Lang;
+using Android.OS;
+
+namespace BasicAuthSample
+{
+    public class LoginCallback : MASCallback
+    {
+        private Activity context;
+        public LoginCallback(Activity activity)
+        {
+            this.context = activity;
+        }
+
+        public override Android.OS.Handler Handler
+        {
+            get
+            {
+                return new Android.OS.Handler(Looper.MainLooper);
+            }
+        }
+
+        public override void OnError(Throwable p0)
+        {
+            Console.WriteLine("Fail Login!!");
+            Console.WriteLine(p0);
+        }
+
+        public override void OnSuccess(Java.Lang.Object user)
+        {
+            Console.WriteLine("Success Login!!");
+            Alert(((MASUser)user).DisplayName, ((MASUser)user).AsJSONObject.ToString(4));
+
+        }
+
+        public void Alert(string Title, string Message)
+        {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+            AlertDialog alert = dialog.Create();
+            alert.SetTitle(Title);
+            alert.SetMessage(Message);
+            alert.SetButton("OK", (c, ev) =>
+            {
+                // Ok button click 
+            });
+            alert.Show();
+        }
+
+    }
+
+}
---
+++ sample/Android/BasicAuthSample/MyAuthenticationListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Android.Content;
+using Com.CA.Mas.Foundation;
+using Com.CA.Mas.Foundation.Auth;
+using Android.App;
+
+namespace BasicAuthSample
+{
+    public class MyAuthenticationListener : Java.Lang.Object, IMASAuthenticationListener
+    {
+        private Activity context;
+        public MyAuthenticationListener(Activity activity)
+        {
+            this.context = activity;
+        }
+
+        public void OnAuthenticateRequest(Context p0, long p1, MASAuthenticationProviders p2)
+        {
+            MASUser.Login("admin", "7layer".ToCharArray(), new LoginCallback(context));
+        }
+
+        public void OnOtpAuthenticateRequest(Context p0, MASOtpAuthenticationHandler p1)
+        {
+            throw new NotImplementedException();
+        }
+
+  
+    }
+}
