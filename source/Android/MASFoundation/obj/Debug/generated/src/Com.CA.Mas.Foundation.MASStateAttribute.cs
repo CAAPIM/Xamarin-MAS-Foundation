6790e07f00cd2261638b187afdbf8f5e99a14ba2
Andy <witan03@ca.com>
2018-01-16 13:29:21 -0800

    Clean up build generated files.

--- source/Android/MASFoundation/obj/Debug/generated/src/Com.CA.Mas.Foundation.MASStateAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Com.CA.Mas.Foundation {
-
-	[global::Android.Runtime.Annotation ("com.ca.mas.foundation.MASState")]
-	public partial class MASStateAttribute : Attribute
-	{
-	}
-}
