c933f296cab48b7029aa3a2bc01dde7de503c123
Stoyan Petrov <stoyan.petrov@ca.com>
2018-01-29 16:28:17 -0800

    Added copyright header to all source files.

--- sample/Android/BasicAuthSample/MainActivity.cs
+++ sample/Android/BasicAuthSample/MainActivity.cs
@@ -1,4 +1,9 @@
-﻿using Android.App;
+﻿// Copyright (c) 2018 CA. All rights reserved.
+// This software is provided under the terms of CA’s Pre-Release Agreement.
+// See the LICENSE [https://github.com/CAAPIM/Xamarin-MAS-Foundation/blob/LICENSE.md] file for details. 
+// This software is for evaluation purposes only and currently not supported by CA.
+
+using Android.App;
 using Android.Widget;
 using Android.OS;
 using Com.CA.Mas.Foundation;
