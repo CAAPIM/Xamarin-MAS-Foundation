6601ee584ec60baf5d44244640ac281fc7e03952
Kieran Halliday <kluhini96@hotmail.com>
2018-02-06 11:04:20 -0800

    Build scripts initial commit

---
+++ source/MASFoundation.iOS/iOSBuild.fsx
@@ -0,0 +1,17 @@
+﻿#r @"packages/FAKE.4.64.4/tools/FakeLib.dll"
+open Fake
+open Fake.XamarinHelper
+
+//Our variables such as project, solution, etc.
+let solution = "MASFoundation.iOS"
+let iOSproject = "MASFoundation.iOS"
+
+Target "MASUnit.ios" (fun () ->
+    iOSBuild (fun defaults ->
+        {defaults with
+            ProjectPath = solution
+            Configuration = "Debug|iPhone"
+            Target = "Build"
+        })
+    TeamCityHelper.PublishArtifact (iOSproject + "/bin/iPhone/Debug/*.ipa")
+)
