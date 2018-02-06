301321a3de70837dbaa51461309bc97f2fe1ca42
Kieran Halliday <kluhini96@hotmail.com>
2018-02-06 14:14:10 -0800

    Debugging iOS build script

--- source/MASFoundation.iOS/iOSBuild.fsx
+++ source/MASFoundation.iOS/iOSBuild.fsx
@@ -1,17 +1,16 @@
 ﻿#r @"packages/FAKE/tools/FakeLib.dll"
 open Fake
-open Fake.XamarinHelper
+open Fake.DotNet.Xamarin
 
 //Our variables such as project, solution, etc.
 let solution = "MASFoundation.iOS"
 let iOSproject = "MASFoundation.iOS"
 
-Target "MASUnit.ios" (fun () ->
+Fake.Core.Target "MASUnit.ios" (fun () ->
     iOSBuild (fun defaults ->
         {defaults with
             ProjectPath = solution
             Configuration = "Debug|iPhone"
             Target = "Build"
         })
-    TeamCityHelper.PublishArtifact (iOSproject + "/bin/iPhone/Debug/*.ipa")
 )
