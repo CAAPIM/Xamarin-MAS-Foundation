b0dd301b1fd5659367bf489f4bf3d78c61c602d9
Jason Wang <jason_wang94@hotmail.com>
2018-03-05 14:52:20 -0800

    Added legal headers

--- sample/Android/BasicAuthSample/UnlockCallback.cs
+++ sample/Android/BasicAuthSample/UnlockCallback.cs
@@ -1,4 +1,9 @@
-﻿using Android.App;
+﻿// Copyright (c) 2018 CA. All rights reserved.
+// This software is provided under the terms of CA’s Pre-Release Agreement.
+// See the LICENSE [https://github.com/CAAPIM/Xamarin-MAS-Foundation/blob/LICENSE.md] file for details. 
+// This software is for evaluation purposes only and currently not supported by CA.
+
+using Android.App;
 using Android.Content;
 using Com.CA.Mas.Foundation;
 using Java.Lang;
@@ -19,7 +24,7 @@ namespace BasicAuthSample
             activity.Alert("Session Unlock", "Session Unlocked Failed!");
         }
 
-        public override void OnSuccess(Java.Lang.Object result)
+        public override void OnSuccess(Object result)
         {
             activity.Alert("Session Unlock", "Session Unlocked!");
         }
