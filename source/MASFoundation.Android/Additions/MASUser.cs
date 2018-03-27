d12056acc100b476dce088dbc6de20c571a77b73
cguzman <cguzman@valorem.com>
2018-03-27 10:02:53 -0700

    Adding login async binding

---
+++ source/MASFoundation.Android/Additions/MASUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Com.CA.Mas.Foundation
+{
+    public partial class MASUser
+    {
+        
+        public static Task<IMASResponse> Login(IMASAuthCredentials credentials) {
+            var tcs = new TaskCompletionSource<IMASResponse>();
+            Login(credentials, new MASUserCompletion<IMASResponse>(tcs));
+
+            return tcs.Task;
+        }
+
+        private class MASUserCompletion<T> : MASCallback where T: class
+        {
+            private readonly TaskCompletionSource<T> _tcs;
+
+            public MASUserCompletion(TaskCompletionSource<T> tcs)
+            {
+                _tcs = tcs;
+            }
+
+            public override void OnSuccess(Java.Lang.Object response)
+            {
+                _tcs.SetResult(response as T);
+            }
+
+            public override void OnError(Java.Lang.Throwable t)
+            {
+                _tcs.SetException(t);
+            }
+        }
+    }
+}
