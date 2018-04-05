44856f4b6715d36a7c7170f46da1370fac787805
Andy <witan03@ca.com>
2018-04-05 09:57:50 -0700

    Update API Reference

--- source/MASFoundation.Android/Additions/MAS.cs
+++ source/MASFoundation.Android/Additions/MAS.cs
@@ -1,34 +1,57 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿    using System.Threading.Tasks;
 
-namespace Com.CA.Mas.Foundation
-{
-    public partial class MAS
+    namespace Com.CA.Mas.Foundation
     {
-        public static Task<IMASResponse> InvokeAsync(IMASRequest request) {
-            var tcs = new TaskCompletionSource<IMASResponse>();
-            Invoke(request, new MASCompletion<IMASResponse>(tcs));
-
-            return tcs.Task;
-        }
-
-        public static Task<Java.Lang.Void> StartAsync(Android.Content.Context context, Java.Net.URL url)
+        public partial class MAS
         {
-            var tcs = new TaskCompletionSource<Java.Lang.Void>();
-            Start(context, url, new MASCompletion<Java.Lang.Void>(tcs));
+            /// <summary>
+            /// Request method for an HTTP POST, PUT, DELETE, GET call to the Gateway.
+            /// </summary>
+            /// <returns>The async task which include the IMASResponse object</returns>
+            /// <param name="request">The request to send</param>
+            public static Task<IMASResponse> InvokeAsync(IMASRequest request) {
+                var tcs = new TaskCompletionSource<IMASResponse>();
+                    Invoke(request, new MASCompletion<IMASResponse>(tcs));
 
-            return tcs.Task;
+                return tcs.Task;
+            }
+
+            /// <summary>
+            /// Starts the lifecycle of the MAS processes with given JSON configuration enrolment URL or null.
+            /// This method will overwrite JSON configuration(if they are different) that was stored in keychain when configuration file path or enrolment URL is provided.
+            /// When URL is recognized as null, this method will initialize SDK by using last used JSON configuration that is stored,
+            ///or load JSON configuration from defined default configuration file name.
+            /// Enrolment URL is an URL from gateway containing some of credentials required to establish secure connection.
+            /// The gateway must be configured to generate and handle enrolment process with client side SDK.
+            /// The enrolment URL can be retrieved in many ways which has to be configured properly along with the gateway in regards of the enrolment process.
+            /// MASFoundation SDK does not request, or retrieve the enrolment URL by itself..
+            /// </summary>
+            /// <returns>The async.</returns>
+            /// <param name="context">Context.</param>
+            /// <param name="url">The enrollment URL If the enrollment url is null, MAS.Start(Context) will be used to start the
+            ///             lilifecycle of the MAS processes.
+            
+            public static Task<Java.Lang.Void> StartAsync(Android.Content.Context context, Java.Net.URL url)
+            {
+                var tcs = new TaskCompletionSource<Java.Lang.Void>();
+                Start(context, url, new MASCompletion<Java.Lang.Void>(tcs));
+
+                return tcs.Task;
+            }
+
+            /// <summary>
+            /// Retrieves a boolean indicating if the gateway is currently reachable or not.
+            /// </summary>
+            /// <returns>True if the gateway is reaachable else return false.</returns>
+            public static Task<Java.Lang.Boolean> GatewayIsReachableAsync()
+            {
+                var tcs = new TaskCompletionSource<Java.Lang.Boolean>();
+                GatewayIsReachable(new MASCompletion<Java.Lang.Boolean>(tcs));
+
+                return tcs.Task;
+            }
+
+
+
         }
-
-        public static Task<Java.Lang.Boolean> GatewayIsReachableAsync()
-        {
-            var tcs = new TaskCompletionSource<Java.Lang.Boolean>();
-            GatewayIsReachable(new MASCompletion<Java.Lang.Boolean>(tcs));
-
-            return tcs.Task;
-        }
-
-
-
     }
-}
--- source/MASFoundation.Android/Additions/MASDevice.cs
+++ source/MASFoundation.Android/Additions/MASDevice.cs
@@ -5,7 +5,26 @@ namespace Com.CA.Mas.Foundation
 {
     public partial class MASDevice
     {
-        
+        /// <summary>
+        ///Deregisters the application resources on this device. This is a two step operation.</p>
+        ///It will attempt to remove the device's registered record in the cloud, and wipe the device
+        ///of all credential settings. If it fails, an error is returned.
+
+        ///A call to deregister during the same session that the device was initially registered will
+        ///fail and may leave the device in a strange state.  This call should work after the
+        ///initial registration and a restart of the application.
+
+        ///A successful call to deregistration will leave the current application session
+        ///without required credentials and settings. You must restart the application
+        ///to re-register the application and retrieve new credentials.
+      
+        ///This should be considered an advanced feature. If you make this available to end users, it
+        ///should not be made too easily accessible and the UI control should be marked and possibly labeled
+        ///to accentuate that it will wipe settings both in the cloud and locally on the device. We
+        ///recommend that you also provide an additional user confirmation UI component to make it clear to
+        ///the user what will occur and allow them to cancel the operation or proceed.
+        /// </summary>
+        /// <returns>The async task</returns>
         public Task<Java.Lang.Void> DeregisterAsync() {
             var tcs = new TaskCompletionSource<Java.Lang.Void>();
             Deregister(new MASCompletion<Java.Lang.Void>(tcs));
