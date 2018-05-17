44f6137e1d024f713fff222be6a5287d62687183
Robert Weber <robert.weber@.com>
2018-05-17 12:18:25 -0700

    Removing Libs/ Removing API References/ Adding Assembly Info

--- source/MASFoundation.Android/Properties/AssemblyInfo.cs
+++ source/MASFoundation.Android/Properties/AssemblyInfo.cs
@@ -11,10 +11,10 @@ using Android.App;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("MASFoundation.Android")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The core library that handles the communication and authentication layer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("CA Technologies")]
+[assembly: AssemblyProduct("CA Mobile SDK")]
 [assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -23,7 +23,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.7.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
--- source/MASFoundation.iOS/Properties/AssemblyInfo.cs
+++ source/MASFoundation.iOS/Properties/AssemblyInfo.cs
@@ -18,10 +18,10 @@ using Foundation;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("MASFoundation.iOS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The core library that handles the communication and authentication layer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("CA Technologies")]
+[assembly: AssemblyProduct("CA Mobile SDK")]
 [assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -30,7 +30,7 @@ using Foundation;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.7.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
