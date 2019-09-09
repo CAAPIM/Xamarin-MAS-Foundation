7af3e5abc2ef4948e7eaedce9af049dc9d1f66c5
as657674 <aditya.shekhar@broadcom.com>
2019-09-09 12:17:33 +0530

    Add progress bar for multipart file upload

--- sample/iOS/BasicAuthSample/ViewController.cs
+++ sample/iOS/BasicAuthSample/ViewController.cs
@@ -50,7 +50,86 @@ namespace BasicAuthSample
         partial void MultiPartFormButton_TouchUpInside(UIButton sender)
         {
 
-            SampleActions.PostMultiPartForm();
+            //SampleActions.PostMultiPartForm();
+            if (MAS.MASState != MASState.DidStart)
+            {
+                Alert("MAS.Invoke", "You must initialize the SDK before calling API");
+            }
+            else
+            {
+                try
+                {
+                    //  Create MASRequestBuilder with HTTP method 
+                    MASRequestBuilder requestBuilder = new MASRequestBuilder("POST");
+                    //
+                    //  Specify an endpoint path, any parameters or headers, and request/response type
+                    //
+                    requestBuilder.EndPoint = "/test/multipart/";
+                    requestBuilder.RequestType = MASRequestResponseType.FormData;
+                    requestBuilder.Header = new NSDictionary("test", "test");
+                    requestBuilder.ResponseType = MASRequestResponseType.TextPlain;
+                    //  Build MASRequestBuilder to convert into MASRequest object
+                    MASRequest request = requestBuilder.Build();
+
+                    NSUrl fileURL = null;
+
+                    UIImagePickerController picker = new UIImagePickerController();
+                    picker = new UIImagePickerController();
+
+                    // set our source to the photo library
+                    picker.SourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+
+                    // set what media types
+                    picker.MediaTypes = UIImagePickerController.AvailableMediaTypes(UIImagePickerControllerSourceType.PhotoLibrary);
+
+                    picker.FinishedPickingMedia += (object sender1, UIImagePickerMediaPickedEventArgs e) =>
+                    {
+                        Console.WriteLine("Image url is - " + e.ImageUrl);
+                        fileURL = e.ImageUrl;
+                        picker.DismissModalViewController(true);
+                        MAS.PostMultiPartForm(request, (formData) =>
+                        {
+                            NSError error = null;
+                            formData.appendPartWithFileURL(fileURL, "aditya.jpg", out error);
+                        }, (prog) =>
+                        {
+                            InvokeOnMainThread(() =>
+                            {
+                                progressBar.Hidden = false;
+                                progressBar.Progress = (float)prog.FractionCompleted;
+                            });
+
+                        }, (response, responseObject, error) =>
+                        {
+                            if (error != null)
+                            {
+                                progressBar.Hidden = true;
+                                //  If an error was returned
+                                Console.WriteLine("Error: {0}", error.LocalizedDescription);
+                                Alert("MAS.PostMultipartForm", error.LocalizedDescription);
+                            }
+                            else if (responseObject != null)
+                            {
+                                //  If a response is returned
+                                progressBar.Hidden = true;
+                                Console.WriteLine("Success: {0}", responseObject.Description);
+                                Alert("MAS.PostMultipartForm", responseObject.Description);
+                            }
+                        });
+                    };
+                    picker.Canceled += (sender1, evt) =>
+                    {
+                        Console.WriteLine("picker cancelled");
+                        picker.DismissModalViewController(true);
+                    };
+                    UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(picker, true, null);
+                }
+                catch (Exception ex)
+                {
+                    Alert("MAS.PostMultipartForm", "MAS PostMultiPartForm could not be completed successfully. Check log to see the exception details.");
+                    Console.WriteLine("MAS PostMultiPartForm could not be completed successfully. ERROR: " + ex.Message);
+                }
+            }
         }
 
 
@@ -96,6 +175,8 @@ namespace BasicAuthSample
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+
+            progressBar.Hidden = true;
 
             //MAS.SetGatewayNetworkActivityLogging(true);
 
@@ -160,6 +241,20 @@ namespace BasicAuthSample
             base.DidReceiveMemoryWarning();
 
             // Release any cached data, images, etc that aren't in use.
+        }
+
+        //
+        // Display a dialog to the user 
+        //
+        public static void Alert(string title, string message)
+        {
+            UIAlertView alert = new UIAlertView()
+            {
+                Title = title,
+                Message = message
+            };
+            alert.AddButton("OK");
+            alert.Show();
         }
 
 
--- sample/iOS/BasicAuthSample/ViewController.designer.cs
+++ sample/iOS/BasicAuthSample/ViewController.designer.cs
@@ -1,149 +1,146 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
 
 namespace BasicAuthSample
 {
-    [Register ("ViewController")]
-    partial class ViewController
-    {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton DeregisterButton { get; set; }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton DeregisterButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton GrantFlowToClientCredentials { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton GrantFlowToClientCredentials { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton GrantFlowToPassword { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton GrantFlowToPassword { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton InvokeAPIButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton InvokeAPIButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton LockButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton LockButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton LoginButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton LoginButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton LogoutButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton LogoutButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton MultiPartFormButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton MultiPartFormButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton StartSDKButton { get; set; }
+		[Outlet]
+		UIKit.UIProgressView progressBar { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton UnlockButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton StartSDKButton { get; set; }
 
-        [Action ("DeregisterButton_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void DeregisterButton_TouchUpInside (UIKit.UIButton sender);
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton UnlockButton { get; set; }
 
-        [Action ("GrantFlowToClientCredentials_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void GrantFlowToClientCredentials_TouchUpInside (UIKit.UIButton sender);
+		[Action ("DeregisterButton_TouchUpInside:")]
+		partial void DeregisterButton_TouchUpInside (UIKit.UIButton sender);
 
-        [Action ("GrantFlowToPassword_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void GrantFlowToPassword_TouchUpInside (UIKit.UIButton sender);
+		[Action ("GrantFlowToClientCredentials_TouchUpInside:")]
+		partial void GrantFlowToClientCredentials_TouchUpInside (UIKit.UIButton sender);
 
-        [Action ("InvokeAPIButton_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void InvokeAPIButton_TouchUpInside (UIKit.UIButton sender);
+		[Action ("GrantFlowToPassword_TouchUpInside:")]
+		partial void GrantFlowToPassword_TouchUpInside (UIKit.UIButton sender);
 
-        [Action ("LockButton_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void LockButton_TouchUpInside (UIKit.UIButton sender);
+		[Action ("InvokeAPIButton_TouchUpInside:")]
+		partial void InvokeAPIButton_TouchUpInside (UIKit.UIButton sender);
 
-        [Action ("LoginButton_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void LoginButton_TouchUpInside (UIKit.UIButton sender);
+		[Action ("LockButton_TouchUpInside:")]
+		partial void LockButton_TouchUpInside (UIKit.UIButton sender);
 
-        [Action ("LogoutButton_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void LogoutButton_TouchUpInside (UIKit.UIButton sender);
+		[Action ("LoginButton_TouchUpInside:")]
+		partial void LoginButton_TouchUpInside (UIKit.UIButton sender);
 
-        [Action ("MultiPartFormButton_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void MultiPartFormButton_TouchUpInside (UIKit.UIButton sender);
+		[Action ("LogoutButton_TouchUpInside:")]
+		partial void LogoutButton_TouchUpInside (UIKit.UIButton sender);
 
-        [Action ("StartSDKButton_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void StartSDKButton_TouchUpInside (UIKit.UIButton sender);
+		[Action ("MultiPartFormButton_TouchUpInside:")]
+		partial void MultiPartFormButton_TouchUpInside (UIKit.UIButton sender);
 
-        [Action ("UnlockButton_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void UnlockButton_TouchUpInside (UIKit.UIButton sender);
+		[Action ("StartSDKButton_TouchUpInside:")]
+		partial void StartSDKButton_TouchUpInside (UIKit.UIButton sender);
 
-        void ReleaseDesignerOutlets ()
-        {
-            if (DeregisterButton != null) {
-                DeregisterButton.Dispose ();
-                DeregisterButton = null;
-            }
+		[Action ("UnlockButton_TouchUpInside:")]
+		partial void UnlockButton_TouchUpInside (UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (DeregisterButton != null) {
+				DeregisterButton.Dispose ();
+				DeregisterButton = null;
+			}
 
-            if (GrantFlowToClientCredentials != null) {
-                GrantFlowToClientCredentials.Dispose ();
-                GrantFlowToClientCredentials = null;
-            }
+			if (GrantFlowToClientCredentials != null) {
+				GrantFlowToClientCredentials.Dispose ();
+				GrantFlowToClientCredentials = null;
+			}
 
-            if (GrantFlowToPassword != null) {
-                GrantFlowToPassword.Dispose ();
-                GrantFlowToPassword = null;
-            }
+			if (GrantFlowToPassword != null) {
+				GrantFlowToPassword.Dispose ();
+				GrantFlowToPassword = null;
+			}
 
-            if (InvokeAPIButton != null) {
-                InvokeAPIButton.Dispose ();
-                InvokeAPIButton = null;
-            }
+			if (InvokeAPIButton != null) {
+				InvokeAPIButton.Dispose ();
+				InvokeAPIButton = null;
+			}
 
-            if (LockButton != null) {
-                LockButton.Dispose ();
-                LockButton = null;
-            }
+			if (LockButton != null) {
+				LockButton.Dispose ();
+				LockButton = null;
+			}
 
-            if (LoginButton != null) {
-                LoginButton.Dispose ();
-                LoginButton = null;
-            }
+			if (LoginButton != null) {
+				LoginButton.Dispose ();
+				LoginButton = null;
+			}
 
-            if (LogoutButton != null) {
-                LogoutButton.Dispose ();
-                LogoutButton = null;
-            }
+			if (LogoutButton != null) {
+				LogoutButton.Dispose ();
+				LogoutButton = null;
+			}
 
-            if (MultiPartFormButton != null) {
-                MultiPartFormButton.Dispose ();
-                MultiPartFormButton = null;
-            }
+			if (MultiPartFormButton != null) {
+				MultiPartFormButton.Dispose ();
+				MultiPartFormButton = null;
+			}
 
-            if (StartSDKButton != null) {
-                StartSDKButton.Dispose ();
-                StartSDKButton = null;
-            }
+			if (StartSDKButton != null) {
+				StartSDKButton.Dispose ();
+				StartSDKButton = null;
+			}
 
-            if (UnlockButton != null) {
-                UnlockButton.Dispose ();
-                UnlockButton = null;
-            }
-        }
-    }
+			if (UnlockButton != null) {
+				UnlockButton.Dispose ();
+				UnlockButton = null;
+			}
+
+			if (progressBar != null) {
+				progressBar.Dispose ();
+				progressBar = null;
+			}
+		}
+	}
 }
